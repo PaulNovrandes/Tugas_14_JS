c317cc9c4f383d52ca40497f91e69013f2d58ac2
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-06 17:29:32 +0700

    Tugas 14 JS
    Pembulatan dengan Ceil, floor dan round. Tugas ini dikumpulkan kepada niomic

---
+++ Tugas14.js
@@ -0,0 +1,11 @@
+function pembagian(){
+  var x = 37;
+  var y = 10;
+
+  hasil = x/y;
+  return hasil;
+}
+
+console.log("Hasil Pembulatan Ceil = ",Math.ceil(pembagian()));
+console.log("Hasil Pebulatan Floor = ",Math.floor(pembagian()));
+console.log("Hasil Pebulatan Round = ",Math.round(pembagian()));
